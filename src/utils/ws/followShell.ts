d015fb1aeeec1d0ac7c64b468db198dbb3ba16c4
eirikhanasand <eirik.hanasand@gmail.com>
2025-11-04 00:21:14 +0100

    Added error handling

--- src/utils/ws/followShell.ts
+++ src/utils/ws/followShell.ts
@@ -7,41 +7,46 @@ export const shellClients = new Map<string, Set<WebSocket>>()
 const messageBuffer: Buffer[] = []
 
 export default function followTerminal(id: string, connection: WebSocket) {
-    const internalWs = new WebSocket(`${config.internal_wss}${id}`, {
-        headers: {
-            'User-Agent': 'hanasand_internal'
-        }
-    })
+    try {
+        const internalWs = new WebSocket(`${config.internal_wss}${id}`, {
+            headers: {
+                'User-Agent': 'hanasand_internal'
+            }
+        })
 
-    registerClient(id, connection, shellClients)
+        registerClient(id, connection, shellClients)
 
-    internalWs.on('message', (msg) => {
-        connection.send(msg)
-    })
+        internalWs.on('message', (msg) => {
+            connection.send(msg)
+        })
 
-    internalWs.on('open', () => {
-        messageBuffer.forEach((msg) => internalWs.send(msg))
-        messageBuffer.length = 0
-    })
+        internalWs.on('open', () => {
+            messageBuffer.forEach((msg) => internalWs.send(msg))
+            messageBuffer.length = 0
+        })
 
-    connection.on('message', (msg: Buffer) => {
-        if (internalWs.readyState === WebSocket.OPEN) {
-            internalWs.send(msg)
-        } else {
-            messageBuffer.push(msg)
-        }
-    })
+        connection.on('message', (msg: Buffer) => {
+            if (internalWs.readyState === WebSocket.OPEN) {
+                internalWs.send(msg)
+            } else {
+                messageBuffer.push(msg)
+            }
+        })
 
-    connection.on('close', () => {
-        removeClient(id, connection, shellClients)
-        internalWs.close()
-    })
+        connection.on('close', () => {
+            removeClient(id, connection, shellClients)
+            internalWs.close()
+        })
 
-    internalWs.on('close', () => {
-        try {
-            connection.close()
-        } catch (error) { 
-            console.log(`Error occured while closing connection for id ${id}: ${error}`)
-        }
-    })
+        internalWs.on('close', () => {
+            try {
+                connection.close()
+            } catch (error) { 
+                console.log(`Error occured while closing connection for id ${id}: ${error}`)
+            }
+        })
+    } catch (error) {
+        connection.send(Buffer.from(JSON.stringify(error)))
+        console.log(error)
+    }
 }
