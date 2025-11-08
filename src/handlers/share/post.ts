02502daab4bb026cbb086ac853a482cfa56607d2
eirikhanasand <eirik.hanasand@gmail.com>
2025-11-08 05:33:42 +0100

    Updated parent and type logic

--- src/handlers/share/post.ts
+++ src/handlers/share/post.ts
@@ -3,15 +3,28 @@ import permissionsWrapper from '#utils/auth/permissionsWrapper.ts'
 import getWords from '#utils/getWords.ts'
 import type { FastifyReply, FastifyRequest } from 'fastify'
 
+type PostShareProps = {
+    id?: string
+    name?: string
+    path?: string
+    content?: string
+    parent?: string
+    type?: string
+}
+
 export default async function postShare(req: FastifyRequest, res: FastifyReply) {
     try {
-        const { id, name, path, content, parent } = req.body as { id?: string, name?: string, path?: string; content?: string, parent?: string }
+        const { id, name, path, content, parent, type } = req.body as PostShareProps ?? {}
         const idHeader = req.headers['id']
         const userId = Array.isArray(idHeader) ? idHeader.join('') : idHeader 
         const alias = getWords()
 
         if (!id || !path || !content) {
-            return res.status(400).send({ error: 'Missing required fields: id, path or content' })
+            return res.status(400).send({ error: 'Missing required fields: id, path or content.' })
+        }
+
+        if (type && type !== 'file' && type !== 'folder') {
+            return res.status(400).send({ error: `Invalid file type ('file' / 'folder'), but got ${type}.` })
         }
 
         if (parent) {
@@ -22,8 +35,8 @@ export default async function postShare(req: FastifyRequest, res: FastifyReply) 
         }
 
         const query = `
-            INSERT INTO shares (id, name, path, content, alias${parent ? ', parent' : ''})
-            VALUES ($1, $2, $3, $4, $5${parent ? ', $6' : ''})
+            INSERT INTO shares (id, name, path, content, alias, parent, type)
+            VALUES ($1, $2, $3, $4, $5, $6, $7)
             ON CONFLICT (id)
             DO UPDATE SET
                 path = EXCLUDED.path,
@@ -31,10 +44,8 @@ export default async function postShare(req: FastifyRequest, res: FastifyReply) 
             RETURNING *
         `
 
-        const params = [id, name || id, path, content, alias[0]]
-        parent && params.push(parent)
+        const params = [id, name || id, path, content, alias[0], parent || null, type || 'file']
         const result = await run(query, params)
-
         if (!result || result.rowCount === 0) {
             return res.status(500).send({ error: 'Failed to create share' })
         }
