3de36992469d7c70d06689d4537fe493117bccc8
eirikhanasand <eirik.hanasand@gmail.com>
2025-11-15 19:01:01 +0100

    Added debug log

--- src/handlers/traffic/post.ts
+++ src/handlers/traffic/post.ts
@@ -15,7 +15,7 @@ type PostRequestProps = {
 export default async function postRequest(req: FastifyRequest, res: FastifyReply) {
     try {
         const ua = req.headers['user-agent'] || ''
-        console.log("ua", ua.toString().startsWith('Hanasand Traffic Logger'), ua)
+        console.log("ua", ua.toString().startsWith('Hanasand Traffic Logger'), '-', ua, '-')
         if (!ua.toString().startsWith('Hanasand Traffic Logger')) {
             return res.status(400).send({ error: 'Unauthorized' })
         }
