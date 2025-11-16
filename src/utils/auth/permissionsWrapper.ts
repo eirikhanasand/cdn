a9fa0b850f08c3edf2e86123c0981aadbe9f3ac3
eirikhanasand <eirik.hanasand@gmail.com>
2025-11-16 18:44:10 +0100

    Added safeguard for missing editors array

--- src/utils/auth/permissionsWrapper.ts
+++ src/utils/auth/permissionsWrapper.ts
@@ -11,21 +11,22 @@ type Permissions = {
     alias: string
     path: string
     owner: string
-    editors: string[]
+    editors?: string[]
 }
 
 export default async function permissionsWrapper({ userId, shareId }: PermissionsProps): Promise<{ status: boolean, permissions: string[] | null }> {
     const query = await loadSQL('checkPermissions.sql')
     const result = await run(query, [userId, shareId])
     const data = result.rows[0] as Permissions
+    const editors = Array.isArray(data.editors) ? data.editors : []
 
-    if (data.owner !== userId && !data.editors.includes(userId)) {
+    if (data.owner !== userId && !editors.includes(userId)) {
         return { status: false, permissions: null }
     }
 
     const permissions = []
     const owner = data.owner === userId
-    const editor = data.editors.includes(userId)
+    const editor = editors.includes(userId)
 
     if (owner) {
         permissions.push('owner')
