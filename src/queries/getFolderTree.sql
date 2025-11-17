9df8cfe5667dd870e452fdf82212b1308540c6b1
eirikhanasand <eirik.hanasand@gmail.com>
2025-11-17 03:27:40 +0100

    Updated query to walk up the tree then back down to ensure the full tree is always returned

--- src/queries/getFolderTree.sql
+++ src/queries/getFolderTree.sql
@@ -1,22 +1,34 @@
-WITH RECURSIVE folder_tree AS (
-    SELECT
-        id,
-        alias,
-        name,
-        type,
-        parent
+WITH RECURSIVE 
+
+ancestors AS (
+    SELECT id, alias, name, type, parent
     FROM shares
-    WHERE id = $1::TEXT
+    WHERE id = $1
 
     UNION ALL
 
-    SELECT s.id,
-           s.alias,
-           s.name,
-           s.type,
-           s.parent
+    SELECT s.id, s.alias, s.name, s.type, s.parent
     FROM shares s
-    INNER JOIN folder_tree ft ON s.parent = ft.id
+    JOIN ancestors a ON a.parent = s.id
+),
+
+root AS (
+    SELECT *
+    FROM ancestors
+    WHERE parent IS NULL
+    LIMIT 1
+),
+
+descendants AS (
+    SELECT id, alias, name, type, parent
+    FROM root
+
+    UNION ALL
+
+    SELECT s.id, s.alias, s.name, s.type, s.parent
+    FROM shares s
+    JOIN descendants d ON s.parent = d.id
 )
+
 SELECT *
-FROM folder_tree;
+FROM descendants;
