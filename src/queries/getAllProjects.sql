e7b19cdc9c782e172bf295ca5fa109709849ab9a
eirikhanasand <eirik.hanasand@gmail.com>
2025-11-18 08:52:22 +0100

    Added editor

--- src/queries/getAllProjects.sql
+++ src/queries/getAllProjects.sql
@@ -1,7 +1,9 @@
 SELECT
     s.alias,
+    COALESCE(ARRAY_AGG(DISTINCT e.editor) FILTER (WHERE e.editor IS NOT NULL), '{}') AS editors,
     s.owner
 FROM shares s
+LEFT JOIN LATERAL UNNEST(s.editors) AS e(editor) ON true
 WHERE s.owner IS NOT NULL
 GROUP BY s.alias, s.owner
 HAVING COUNT(*) > 1;
