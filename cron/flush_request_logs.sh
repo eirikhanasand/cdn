47cb065c43c83f94d567498375189f353adb5761
eirikhanasand <eirik.hanasand@gmail.com>
2026-03-22 18:45:10 +0100

    fixed concurrency issue saturating all connections

--- cron/flush_request_logs.sh
+++ cron/flush_request_logs.sh
@@ -16,13 +16,13 @@ INSERT INTO request_logs_all (domain, ip, user_agent, path, method, referer, hit
 SELECT domain, ip, user_agent, path, method, referer, hits, first_seen, last_seen
 FROM request_logs;
 
-DELETE FROM request_logs;
+TRUNCATE TABLE request_logs;
 
 COMMIT;
 EOF
 
 psql "postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/${DB_NAME}" \
-     -c "REFRESH MATERIALIZED VIEW request_logs_combined_mv;"
+     -c "REFRESH MATERIALIZED VIEW request_logs_combined_mv CONCURRENTLY;"
 
 END_TIME=$(date +%s)
 DURATION=$((END_TIME - START_TIME))
