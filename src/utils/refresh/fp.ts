eac1c060eab0b9bf314a87899aa6eec47b166e33
eirikhanasand <eirik.hanasand@gmail.com>
2026-03-22 16:45:02 +0100

    removed excess comments

--- src/utils/refresh/fp.ts
+++ src/utils/refresh/fp.ts
@@ -1,4 +1,3 @@
-// import alertSlowQuery from '#utils/alertSlowQuery.ts'
 import config from '#constants'
 import fp from 'fastify-plugin'
 import ua from './queries/ua.ts'
@@ -8,13 +7,11 @@ import summary from './queries/summary.ts'
 
 export default fp(async (fastify) => {
     async function refreshQueries() {
-        // const start = Date.now()
         const newUA = await ua()
         const newIP = await ip()
         const newTPS = await tps()
         const newSummary = await summary()
-        // const duration = (Date.now() - start) / 1000
-        // alertSlowQuery(duration, 'cache')
+
         fastify.cachedUAMetrics = { status: newUA.status, data: Buffer.from(JSON.stringify(newUA.data)) }
         fastify.cachedIPMetrics = { status: newIP.status, data: Buffer.from(JSON.stringify(newIP.data)) }
         fastify.cachedTPS = { status: newTPS.status, data: Buffer.from(JSON.stringify(newTPS.data)) }
